8cf89c9a10b2e8cbdd63b8e99e98040ac60d33b7
WebDev1119 <warriorjhs@outlook.com>
2019-12-18 16:36:06 +0800

    update code form

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-    return (
-        <div>
-            Hi There!
-        </div>
-    );
+  return (
+    <div>
+      Hi There!
+      <a href="/auth/google">Sign In Google</a>
+    </div>
+  );
 };
 
 export default App;
