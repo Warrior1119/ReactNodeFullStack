d544170f1b09bec32a5b6a3dfe7f33b2e27d741f
WebDev1119 <warriorjhs@outlook.com>
2020-01-07 22:29:19 +0800

    add stripecheckout

---
+++ server/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "1037479350348-uo6rjuqpk2hjdra2gmihphi1gk0qgl72.apps.googleusercontent.com",
+  googleClientSecret: "OhJiDzBuOJFVXKZmHvWFJQ9W",
+  cookieKey: "iasdihfiasdhfisdhfalskdfhqwoehskdalfasdl",
+  stripePublishableKey: "pk_test_8hNTCof8qb0zbgzk7y4SrW5i00ib93gdF6",
+  stripeSecretKey: "sk_test_FPa0SII05RMNb4uS0IPFxoJn00C0ppKdSh"
+};
